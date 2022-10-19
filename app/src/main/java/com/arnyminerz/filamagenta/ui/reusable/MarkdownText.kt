08cd356a343e54aacb93adacf9ab5498fcf639ad
Arnau Mora <arnyminer.z@gmail.com>
2022-10-19 11:50:09 +0200

    :sparkles: Added HR
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/MarkdownText.kt
+++ app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/MarkdownText.kt
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -55,110 +56,115 @@ fun MarkdownText(
     overflow: TextOverflow = TextOverflow.Visible,
     maxLines: Int = Int.MAX_VALUE,
 ) {
-    val annotatedString = buildAnnotatedString {
-        markdown.split('\n').forEach { line ->
-            val headlineIndex = line.indexOf('#')
-            if (headlineIndex >= 0) {
-                // This is header, count depth
-                val regex = Regex("[^#]")
-                var depth = 0
-                while (!regex.matchesAt(line, depth)) depth++
-                val headline = line.substring(depth + 1)
-                val typography = headlineDepthStyles.getOrElse(depth) { TextStyle.Default }
-                withStyle(typography.toSpanStyle()) { append(headline) }
-            } else if (line.startsWith('-')) { // List
-                val item = line.substring(1)
-                append("$Bullet\t$item")
-            } else {
-                var currentStyle = SpanStyle()
-                var c = 0
-                while (c < line.length) withStyle(currentStyle) {
-                    val char = line[c]
-                    val nextChar = c.takeIf { it + 1 < line.length }?.let { line[it + 1] }
-                    if (char == '*' && nextChar == '*') { // Bold
-                        currentStyle = if (currentStyle.fontWeight == FontWeight.Bold)
-                            currentStyle.copy(fontWeight = FontWeight.Normal)
-                        else
-                            currentStyle.copy(fontWeight = FontWeight.Bold)
-                        // Add two since the pointer is double
-                        c += 2
-                    } else if (char == '*') { // Italic
-                        currentStyle = if (currentStyle.fontStyle == FontStyle.Italic)
-                            currentStyle.copy(fontStyle = FontStyle.Normal)
-                        else
-                            currentStyle.copy(fontStyle = FontStyle.Italic)
-                        c++
-                    } else if (char == '~') { // Strikethrough
-                        currentStyle =
-                            if (currentStyle.textDecoration == TextDecoration.LineThrough)
-                                currentStyle.copy(textDecoration = TextDecoration.None)
+    markdown.split('\n').forEach { line ->
+        if (line.startsWith("--")) // If starts with at least two '-', add divider
+            Divider()
+        else {
+            val annotatedString = buildAnnotatedString {
+                val headlineIndex = line.indexOf('#')
+                if (headlineIndex >= 0) {
+                    // This is header, count depth
+                    val regex = Regex("[^#]")
+                    var depth = 0
+                    while (!regex.matchesAt(line, depth)) depth++
+                    val headline = line.substring(depth + 1)
+                    val typography = headlineDepthStyles.getOrElse(depth) { TextStyle.Default }
+                    withStyle(typography.toSpanStyle()) { append(headline) }
+                } else if (line.startsWith('-')) { // List
+                    val item = line.substring(1)
+                    append("$Bullet\t$item")
+                } else {
+                    var currentStyle = SpanStyle()
+                    var c = 0
+                    while (c < line.length) withStyle(currentStyle) {
+                        val char = line[c]
+                        val nextChar = c.takeIf { it + 1 < line.length }?.let { line[it + 1] }
+                        if (char == '*' && nextChar == '*') { // Bold
+                            currentStyle = if (currentStyle.fontWeight == FontWeight.Bold)
+                                currentStyle.copy(fontWeight = FontWeight.Normal)
                             else
-                                currentStyle.copy(textDecoration = TextDecoration.LineThrough)
-                        c++
-                    } else if (char == '_') { // Underline
-                        currentStyle = if (currentStyle.textDecoration == TextDecoration.Underline)
-                            currentStyle.copy(textDecoration = TextDecoration.None)
-                        else
-                            currentStyle.copy(textDecoration = TextDecoration.Underline)
-                        c++
-                    } else if (char == '[') { // Starts a link
-                        // Search for the closing tag
-                        val preClosing = line.indexOf(']', c + 1)
-                        // Search for the actual link start
-                        val lOpen = line.indexOf('(', c + 1)
-                        // And the ending
-                        val lClose = line.indexOf(')', c + 1)
+                                currentStyle.copy(fontWeight = FontWeight.Bold)
+                            // Add two since the pointer is double
+                            c += 2
+                        } else if (char == '*') { // Italic
+                            currentStyle = if (currentStyle.fontStyle == FontStyle.Italic)
+                                currentStyle.copy(fontStyle = FontStyle.Normal)
+                            else
+                                currentStyle.copy(fontStyle = FontStyle.Italic)
+                            c++
+                        } else if (char == '~') { // Strikethrough
+                            currentStyle =
+                                if (currentStyle.textDecoration == TextDecoration.LineThrough)
+                                    currentStyle.copy(textDecoration = TextDecoration.None)
+                                else
+                                    currentStyle.copy(textDecoration = TextDecoration.LineThrough)
+                            c++
+                        } else if (char == '_') { // Underline
+                            currentStyle =
+                                if (currentStyle.textDecoration == TextDecoration.Underline)
+                                    currentStyle.copy(textDecoration = TextDecoration.None)
+                                else
+                                    currentStyle.copy(textDecoration = TextDecoration.Underline)
+                            c++
+                        } else if (char == '[') { // Starts a link
+                            // Search for the closing tag
+                            val preClosing = line.indexOf(']', c + 1)
+                            // Search for the actual link start
+                            val lOpen = line.indexOf('(', c + 1)
+                            // And the ending
+                            val lClose = line.indexOf(')', c + 1)
 
-                        // Check if link is valid
-                        if (preClosing < 0 || lOpen < 0 || lClose < 0 || lOpen > lClose || preClosing > lOpen) {
+                            // Check if link is valid
+                            if (preClosing < 0 || lOpen < 0 || lClose < 0 || lOpen > lClose || preClosing > lOpen) {
+                                append(char)
+                                c++
+                            } else {
+                                val link = line.substring(lOpen + 1, lClose)
+                                val text = line.substring(c + 1, preClosing)
+                                pushStringAnnotation(
+                                    tag = "link",
+                                    annotation = link,
+                                )
+                                withStyle(
+                                    currentStyle.copy(
+                                        textDecoration = TextDecoration.Underline,
+                                        color = Color(LinkColor),
+                                    ),
+                                ) { append(text) }
+                                pop()
+                                c = lClose + 1
+                            }
+                        } else {
                             append(char)
                             c++
-                        } else {
-                            val link = line.substring(lOpen + 1, lClose)
-                            val text = line.substring(c + 1, preClosing)
-                            pushStringAnnotation(
-                                tag = "link",
-                                annotation = link,
-                            )
-                            withStyle(
-                                currentStyle.copy(
-                                    textDecoration = TextDecoration.Underline,
-                                    color = Color(LinkColor),
-                                ),
-                            ) { append(text) }
-                            pop()
-                            c = lClose + 1
                         }
-                    } else {
-                        append(char)
-                        c++
                     }
                 }
+                append('\n')
             }
-            append('\n')
+
+            val uriHandler = LocalUriHandler.current
+            ClickableText(
+                text = annotatedString,
+                modifier = modifier,
+                overflow = overflow,
+                maxLines = maxLines,
+                style = style,
+                softWrap = softWrap,
+                onClick = {
+                    annotatedString
+                        .getStringAnnotations("link", it, it)
+                        .firstOrNull()?.let { stringAnnotation ->
+                            try {
+                                uriHandler.openUri(stringAnnotation.item)
+                            } catch (e: ActivityNotFoundException) {
+                                Timber.e(e, "Could not find link handler.")
+                            }
+                        }
+                }
+            )
         }
     }
-
-    val uriHandler = LocalUriHandler.current
-    ClickableText(
-        text = annotatedString,
-        modifier = modifier,
-        overflow = overflow,
-        maxLines = maxLines,
-        style = style,
-        softWrap = softWrap,
-        onClick = {
-            annotatedString
-                .getStringAnnotations("link", it, it)
-                .firstOrNull()?.let { stringAnnotation ->
-                    try {
-                        uriHandler.openUri(stringAnnotation.item)
-                    } catch (e: ActivityNotFoundException) {
-                        Timber.e(e, "Could not find link handler.")
-                    }
-                }
-        }
-    )
 }
 
 @Preview
@@ -182,7 +188,9 @@ fun MarkdownTextPreview() {
                     "- Lists\n" +
                     "- are\n" +
                     "- also\n" +
-                    "- supported\n",
+                    "- supported\n" +
+                    "--------\n" +
+                    "That is a hr!",
             modifier = Modifier
                 .padding(horizontal = 8.dp),
         )
