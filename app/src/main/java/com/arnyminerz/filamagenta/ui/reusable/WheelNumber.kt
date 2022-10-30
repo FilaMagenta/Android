d154fc107e41df6a9cc7c1e2505310b8a707073c
Arnau Mora <arnyminer.z@gmail.com>
2022-10-30 21:49:59 +0100

    :sparkles: Added wheel lock icon
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/WheelNumber.kt
+++ app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/WheelNumber.kt
@@ -1,18 +1,26 @@
 package com.arnyminerz.filamagenta.ui.reusable
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.arnyminerz.filamagenta.R
 import com.arnyminerz.filamagenta.data.account.Wheel
 
 @Composable
@@ -30,14 +38,24 @@ fun RowScope.WheelNumber(@StringRes label: Int, wheel: Wheel?) {
                 .fillMaxWidth()
                 .padding(top = 4.dp),
         )
-        Text(
-            wheel?.number?.toString() ?: "--",
-            style = MaterialTheme.typography.labelLarge,
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 4.dp),
-        )
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                wheel?.number?.toString() ?: "--",
+                style = MaterialTheme.typography.labelLarge,
+                fontSize = 30.sp,
+            )
+            if (wheel?.locked == true)
+                Icon(
+                    imageVector = Icons.Rounded.Lock,
+                    contentDescription = stringResource(R.string.account_wheel_lock),
+                    modifier = Modifier.size(16.dp),
+                )
+        }
     }
 }
