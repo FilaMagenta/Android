bc87f3d2222a8be9e6e3884416355cca9f8439a8
Arnau Mora <arnyminer.z@gmail.com>
2022-11-10 20:33:52 +0100

    :sparkles: Keyboard capitalization
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/PersonalDataField.kt
+++ app/src/main/java/com/arnyminerz/filamagenta/ui/reusable/PersonalDataField.kt
@@ -3,6 +3,7 @@ package com.arnyminerz.filamagenta.ui.reusable
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arnyminerz.filamagenta.R
@@ -57,6 +59,7 @@ fun LabeledTextField(
         supportingText = supportingTextRes?.let { { Text(stringResource(supportingTextRes)) } },
         trailingIcon = trailing,
         colors = colors,
+        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
     )
 }
 
